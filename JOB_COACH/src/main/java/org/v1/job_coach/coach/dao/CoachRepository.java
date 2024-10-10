22a3cac4413679cd67955d26d4c8c66b2bf32c15
ggowater112@naver.com <127701730+haennni@users.noreply.github.com>
2024-10-10 18:25:19 +0900

    ✨ implement get all coaches list API
    - 전체 코치 리스트 반환 API 구현

--- JOB_COACH/src/main/java/org/v1/job_coach/coach/dao/CoachRepository.java
+++ JOB_COACH/src/main/java/org/v1/job_coach/coach/dao/CoachRepository.java
@@ -1,9 +1,13 @@
 package org.v1.job_coach.coach.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.v1.job_coach.coach.domain.Coach;
 
 @Repository
 public interface CoachRepository extends JpaRepository<Coach, Long> {
+
+    Page<Coach> findAllBy(Pageable pageable);
 }
